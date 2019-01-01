090fc4d2baf2822e7f3e990596f3e120e32c7b92
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-01 13:07:42 +0200

    add append method

--- linked_list.py
+++ linked_list.py
@@ -8,3 +8,13 @@ class LinkedList:
     """Linked List representation."""
     def __init__(self, head=None):
         self.head = head
+    
+    def append(self, new_element):
+        """Add new element to the end of the linked list."""
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
