2f1413b6c6aa8753e3da4ea5e8e2f48485ce8a65
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-01 13:52:47 +0200

    set up a linked list

--- linked_list_test.py
+++ linked_list_test.py
@@ -6,3 +6,11 @@ node1 = Node(1)
 node2 = Node(2)
 node3 = Node(3)
 node4 = Node(4)
+
+# Set up a LinkedList
+linked_list = LinkedList(node1)
+linked_list.append(node2)
+linked_list.append(node3)
+
+
+
