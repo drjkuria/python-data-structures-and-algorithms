2929e11e13a11f1933d7a9cf45ef23c40f81340a
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-03 12:47:23 +0200

    add a positive case

--- binary_search.py
+++ binary_search.py
@@ -6,7 +6,7 @@ def binary_search_iterative(input_array, value):
     high = len(input_array) - 1
     while low <= high:
         mid = (low + high) // 2
-        if (input_array[mid] == value):
+        if input_array[mid] == value:
             return mid
         elif input_array[mid] < value:
             low = mid + 1
--- binary_search_test.py
+++ binary_search_test.py
@@ -5,4 +5,6 @@ test_list = [1, 3, 9, 11, 15, 19, 29]
 test_val = 25
 print(binary_search_iterative(test_list, test_val)) # -1, that is, not found
 
+test_val = 15
+print(binary_search_iterative(test_list, test_val)) # 4, that is, 15 found at index 4
 
