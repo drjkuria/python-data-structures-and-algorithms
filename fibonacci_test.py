079570d51fca0b23a254f2f7bb5743248366a4f6
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-03 14:58:03 +0200

    add recursive fibonacci test

--- fibonacci_test.py
+++ fibonacci_test.py
@@ -5,3 +5,9 @@ from fibonacci import get_fib_iterative, get_fib_recursive
 print(get_fib_iterative(0)) # 0
 print(get_fib_iterative(9)) # 34
 print(get_fib_iterative(11)) # 89
+
+# Recursive test
+print(get_fib_recursive(3)) # 2
+print(get_fib_recursive(4)) # 3
+print(get_fib_recursive(5)) # 5
+
