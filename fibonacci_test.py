ce70e6722a9b4ab99abf695bd40f8ec25bcb3368
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-03 14:47:57 +0200

    add iterative fibonacci test

---
+++ fibonacci_test.py
@@ -0,0 +1,7 @@
+from fibonacci import get_fib_iterative, get_fib_recursive
+
+# Test cases
+# Iterative test
+print(get_fib_iterative(0)) # 0
+print(get_fib_iterative(9)) # 34
+print(get_fib_iterative(11)) # 89
