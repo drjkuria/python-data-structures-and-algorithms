6435b371183f3f4191d1b74ffeaf80c64a352184
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-03 12:45:49 +0200

    add a negative test case

---
+++ binary_search_test.py
@@ -0,0 +1,8 @@
+from binary_search import binary_search_iterative
+
+# Test iterative binary_search
+test_list = [1, 3, 9, 11, 15, 19, 29]
+test_val = 25
+print(binary_search_iterative(test_list, test_val)) # -1, that is, not found
+
+
