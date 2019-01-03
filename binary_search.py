43eba59f9640c5dee8260b865ad5b2ca4d557735
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-03 12:31:39 +0200

    add iterative binary search function

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,15 @@
 """Binary search algorithm using iteration."""
 
+def binary_search_iterative(input_array, value):
+    """Iterative binary search."""
+    low = 0
+    high = len(input_array) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if (input_array[mid] == value):
+            return mid
+        elif input_array[mid] < value:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
