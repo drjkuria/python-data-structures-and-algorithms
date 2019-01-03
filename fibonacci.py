be8d594477aba1f976bd49a0f206e4032227f4e8
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-03 14:18:30 +0200

    add fibonacci module

---
+++ fibonacci.py
@@ -0,0 +1,7 @@
+"""Generating Fibonacci sequence using iteration
+and recursion.
+The Fibonacci Sequence follows one rule: get the next number in the
+sequence by adding the previous two numbers, e.g.
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ..."""
+
+
