3e2d90ccb7283facfa513e18a0a4176a7c85799d
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-03 14:39:11 +0200

    add get fib iterative function

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,18 @@ sequence by adding the previous two numbers, e.g.
 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ..."""
 
 
+def get_fib_iterative(position):
+    """Get nth position fibonacci number iteratively."""
+    first = 0
+    second = 1
+    next_fib = first + second
+    if position == 0:
+        return first
+    elif position == 1:
+        return second
+    else:
+        for i in  range(2, position):
+            first = second
+            second = next_fib
+            next_fib = first + second
+    return next_fib
