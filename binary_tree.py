3753cd9e6f63f8ac7e33df21683c09c900b7da78
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-04 13:20:02 +0200

    add search method

--- binary_tree.py
+++ binary_tree.py
@@ -9,3 +9,9 @@ class BinaryTree:
     """Binary tree representation."""
     def __init__(self, root):
         self.root = Node(root)
+    
+    def search(self, find_val):
+        """Return true if find_val is in the tree,
+        otherwise return false."""
+        return self.pre_order_search(tree.root, find_val)
+    
