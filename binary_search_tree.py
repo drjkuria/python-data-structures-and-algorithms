e9e20ee2fb6244f56bbf984a8e2c2ddcc2e245e1
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-04 14:55:43 +0200

    add insert method

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -7,4 +7,18 @@ class Node:
 class BinarySearchTree:
     def __init__(self, root):
         self.root = Node(root)
+
+    def insert(self, new_val):
+        self.insert_helper(self.root, new_val)
     
+    def insert_helper(self, current, new_val):
+        if current.value < new_val:
+            if current.right:
+                self.insert_helper(current.right, new_val)
+            else:
+                current.right = Node(new_val)
+        else:
+            if current.left:
+                self.insert_helper(current.left, new_val)
+            else:
+                current.left = Node(new_val)
