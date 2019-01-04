51c359c4e5bc7963715b70a27e976b6dbce316d2
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-04 09:12:18 +0200

    add quick sort function

--- quicksort.py
+++ quicksort.py
@@ -8,7 +8,13 @@ def partition(input_array, low, high):
     for j in range(low, high):
         if input_array[j] <= pivot:
             i += 1
-            input_array[i], input_array[j] = input_array[j], array[i]
+            input_array[i], input_array[j] = input_array[j], input_array[i]
     input_array[i + 1], input_array[high] = input_array[high], input_array[i + 1]
     return (i + 1)
 
+def quick_sort(input_array, low, high):
+    """Iterative Quick sort."""
+    if low < high:
+        index = partition(input_array, low, high)
+        quick_sort(input_array, low, index - 1)
+        quick_sort(input_array, index + 1, high)
