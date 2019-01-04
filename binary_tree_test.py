c6fc54ae59a0a193789e3b4ad1ce1b5b8626acff
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-04 13:54:53 +0200

    add binary tree setup

---
+++ binary_tree_test.py
@@ -0,0 +1,10 @@
+from binary_tree import BinaryTree, Node
+
+# Set up BinaryTree
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+
+
