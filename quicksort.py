be002d355fdc95fc4931906dcf2672d311ace893
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-04 08:32:00 +0200

    add partition function

--- quicksort.py
+++ quicksort.py
@@ -1,1 +1,14 @@
 """Quick sort algorithm"""
+
+def partition(input_array, low, high):
+    """Partition the array using pivot."""
+    i = (low - 1)
+    pivot = input_array[high]
+
+    for j in range(low, high):
+        if input_array[j] <= pivot:
+            i += 1
+            input_array[i], input_array[j] = input_array[j], array[i]
+    input_array[i + 1], input_array[high] = input_array[high], input_array[i + 1]
+    return (i + 1)
+
