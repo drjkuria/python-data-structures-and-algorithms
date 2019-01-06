84f43a4331d4ad1946da5b1c5b7b64fe71ed8b25
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-06 07:27:58 +0200

    add edge class

--- graph.py
+++ graph.py
@@ -3,3 +3,10 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.edges = []
+
+class Edge:
+    """A connection between two nodes."""
+    def __init__(self, value, node_from, node_to):
+        self.value = value
+        self.node_from = node_from
+        self.node_to = node_to
