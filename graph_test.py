ba8ac03fdeba2c54ad733f4d1a36d5db606e691e
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-06 09:29:54 +0200

    add get adjacency list test

--- graph_test.py
+++ graph_test.py
@@ -10,3 +10,6 @@ graph.insert_edge(103, 3, 4)
 # Test get_edge_list
 print(graph.get_edge_list()) # [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)]
 
+# Test get_adjacency_list
+print(graph.get_adjacency_list()) # [None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None]
+
