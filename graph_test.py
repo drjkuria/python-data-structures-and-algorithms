28624e8eb5cfbc6252f0bbe25ae96edb575f3a0d
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-06 09:23:42 +0200

    add get edge list

--- graph_test.py
+++ graph_test.py
@@ -7,3 +7,6 @@ graph.insert_edge(101, 1, 3)
 graph.insert_edge(102, 1, 4)
 graph.insert_edge(103, 3, 4)
 
+# Test get_edge_list
+print(graph.get_edge_list()) # [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)]
+
