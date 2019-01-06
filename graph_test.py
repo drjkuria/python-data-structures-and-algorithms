a234e359501bae00e8f3de54bc05d5dc3699399f
Jonah Kuria <jonah.kuria@hotmail.com>
2019-01-06 09:33:36 +0200

    add get adjacency matrix

--- graph_test.py
+++ graph_test.py
@@ -13,3 +13,5 @@ print(graph.get_edge_list()) # [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 
 # Test get_adjacency_list
 print(graph.get_adjacency_list()) # [None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None]
 
+# Test get_adjacency_matrix
+print(graph.get_adjacency_matrix()) # [[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]]
